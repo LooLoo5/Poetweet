9856b878c85972c446ab912162e5c70d3801f57b
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2018-12-10 16:28:32 -0600

    fixed lint errors

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,8 +6,9 @@
       <b>haiku</b> is an unrhymed Japanese poetic form that consists of 17 syllables
       arranged in three lines containing five, seven, and five syllables, respectively.
       A haiku expresses much and suggests more in the fewest possible words.
-      <br><br>The form gained distinction in the 17th century, when Basho, a Japanese poet considered the
-      greatest practitioner of the form, elevated it to a highly refined art.
+      <br>
+      <br>The form gained distinction in the 17th century, when Basho, a Japanese poet
+      considered the greatest practitioner of the form, elevated it to a highly refined art.
       It remains Japan’s most popular poetic form. The Imagist poets (1912–30) and others
       have imitated the form in English and other languages.
     </p>
